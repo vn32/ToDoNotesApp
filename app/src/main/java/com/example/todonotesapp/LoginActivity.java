c722faf74ca06e95136fbefce3108983784beeca
vn32 <nishadvikas87@gmial.com>
2020-04-13 15:31:59 +0530

    MyNotesActivity Added

--- app/src/main/java/com/example/todonotesapp/LoginActivity.java
+++ app/src/main/java/com/example/todonotesapp/LoginActivity.java
@@ -2,13 +2,37 @@ package com.example.todonotesapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class LoginActivity extends AppCompatActivity {
+    EditText EditTextUserName,EditTextFullName;
+    Button buttonLoginId;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        //mapping of variable with id of widges
+        EditTextFullName=findViewById(R.id.EditTextFullName);
+        EditTextUserName=findViewById(R.id.EditTextUserName);
+        buttonLoginId=findViewById(R.id.buttonLoginId);
+        View.OnClickListener clickAction=new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String FullName=EditTextFullName.getText().toString();
+                Intent intent=new Intent(LoginActivity.this,MyNotesActivity.class);
+                intent.putExtra("full_name",FullName);
+                startActivity(intent);
+
+            }
+        };
+        buttonLoginId.setOnClickListener(clickAction);
+
+
     }
 }
---
+++ app/src/main/java/com/example/todonotesapp/MyNotesActivity.java
@@ -0,0 +1,19 @@
+package com.example.todonotesapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+
+public class MyNotesActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_my_notes);
+        Intent intent=getIntent();
+        intent.getStringExtra("full_name");
+        Log.d("MyNotesActivity",intent.getStringExtra("full_name"));
+    }
+}
