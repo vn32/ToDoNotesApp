ac8741f31064329ab8cf19d94618592407886b6c
vn32 <nishadvikas87@gmial.com>
2020-04-13 15:58:06 +0530

    Login Screen condition Added

--- app/src/main/java/com/example/todonotesapp/LoginActivity.java
+++ app/src/main/java/com/example/todonotesapp/LoginActivity.java
@@ -4,10 +4,12 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
 public class LoginActivity extends AppCompatActivity {
     EditText EditTextUserName,EditTextFullName;
@@ -25,9 +27,15 @@ public class LoginActivity extends AppCompatActivity {
             @Override
             public void onClick(View view) {
                 String FullName=EditTextFullName.getText().toString();
-                Intent intent=new Intent(LoginActivity.this,MyNotesActivity.class);
-                intent.putExtra("full_name",FullName);
-                startActivity(intent);
+                String UserName=EditTextUserName.getText().toString();
+                //checking either of Fullname + username are empty or not by TextUtils.isEmpty()
+                if(!TextUtils.isEmpty(FullName) && !TextUtils.isEmpty(UserName)) {
+                    Intent intent = new Intent(LoginActivity.this, MyNotesActivity.class);
+                    intent.putExtra("full_name", FullName);
+                    startActivity(intent);
+                } else {
+                    Toast.makeText(LoginActivity.this, "FullName and UserName can't be empty", Toast.LENGTH_SHORT).show();
+                }
 
             }
         };
--- app/src/main/java/com/example/todonotesapp/MyNotesActivity.java
+++ app/src/main/java/com/example/todonotesapp/MyNotesActivity.java
@@ -7,13 +7,16 @@ import android.os.Bundle;
 import android.util.Log;
 
 public class MyNotesActivity extends AppCompatActivity {
+    String fullname;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_my_notes);
         Intent intent=getIntent();
-        intent.getStringExtra("full_name");
-        Log.d("MyNotesActivity",intent.getStringExtra("full_name"));
+        //assign value to varibale fullname of data accessed
+        fullname=intent.getStringExtra("full_name");
+        //to access the action bar and replace it by value obtained
+        getSupportActionBar().setTitle(fullname);
     }
 }
