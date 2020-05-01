4a1cb46f86ab76bd1d3768209a626ffd13f58b84
vn32 <nishadvikas87@gmial.com>
2020-05-01 09:34:41 +0530

    notification chaneel implemented successfully

--- app/src/main/java/com/example/todonotesapp/fcm/myFirebaseMessagingService.kt
+++ app/src/main/java/com/example/todonotesapp/fcm/myFirebaseMessagingService.kt
@@ -1,6 +1,13 @@
 package com.example.todonotesapp.fcm
 
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.content.Context
+import android.media.RingtoneManager
+import android.os.Build
 import android.util.Log
+import androidx.core.app.NotificationCompat
+import com.example.todonotesapp.R
 import com.google.firebase.messaging.FirebaseMessagingService
 import com.google.firebase.messaging.RemoteMessage
 
@@ -9,6 +16,25 @@ class MyFirebaseMessagingServices:FirebaseMessagingService(){
         super.onMessageReceived(message)
         Log.d("Firebase",message.from)
         Log.d("Firebase",message.notification?.body)
+        setUpNotificationMessage(message.notification?.body)
+    }
+
+    private fun setUpNotificationMessage(body: String?) {
+        val channelId="Todo Id"
+        val ringtone = RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION)
+        val notificationBuilder = NotificationCompat.Builder(this, channelId)
+                .setSmallIcon(R.mipmap.ic_launcher_round)
+                .setContentTitle("ToDo Notes App")
+                .setContentText(body)
+                .setSound(ringtone)
+        val notificationManager = getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+
+        if (Build.VERSION.SDK_INT > Build.VERSION_CODES.O) {
+            val channel = NotificationChannel(channelId, "ToDo-Notes", NotificationManager.IMPORTANCE_HIGH)
+            notificationManager.createNotificationChannel(channel)
+        }
+        notificationManager.notify(0, notificationBuilder.build())
+
     }
 
     override fun onNewToken(token: String) {
